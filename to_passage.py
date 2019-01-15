49efc1ea33a6ff108392031b5c04ed9a7ff1b785
Dian Yu <dy648@nyu.edu>
2019-01-15 11:06:11 -0800

    added linearization to passage testing file

---
+++ to_passage.py
@@ -0,0 +1,5 @@
+linearized = "[ Additionally] [ ,] [ [ [ [ Carey] [ 's] ] [ [ newly] " \
+             "[ slimmed] ] [ figure] ] [ began] [ to] [ change] ] [ ,] [ as]" \
+             " [ [ she] [ stopped] [ [ her] [ exercise] [ routines] ] ] [ and]" \
+             " [ [ she] [ gained] [ weight] [ .] ]"
+
