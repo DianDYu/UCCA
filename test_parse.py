e2b8a0d1bc0fc5ce9d791b706d4991b65cbd8b94
Dian Yu <diayu@ucdavis.edu>
2019-01-22 14:35:09 -0800

    update test_parse file

--- test_parse.py
+++ test_parse.py
@@ -4,7 +4,7 @@ from parse import *
 dev_file_dir = "dev_proc.pt"
 vocab_dir = "vocab.pt"
 
-checkpoint_path = "models/epoch_250_f1_0.86s.pt"
+checkpoint_path = "/home/dianyu/Desktop/P/UCCA/models/epoch_35_f1_77.68.pt"
 
 
 def main():
@@ -14,12 +14,21 @@ def main():
 
     model_r, attn_r = load_test_model(checkpoint_path)
 
-    model_r.eval()
-    attn_r.eval()
-
     f1 = get_validation_accuracy(dev_text_tensor, model_r, attn_r, dev_text, dev_passages, dev_pos)
 
     print("evaluated on %d passages" % len(dev_passages))
     print("micro F1: %.4f " % f1)
 
 main()
+
+
+
+
+
+
+def process_test_data(folder_name):
+    # 20k out of domain data
+    folder_name = "/home/dianyu/Downloads/xml"
+    dev_passages = list(read_passages(folder_name))\
+
+    # vocab is the problem (unk words)
