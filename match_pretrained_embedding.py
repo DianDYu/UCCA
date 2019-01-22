a128e0f8f6eddaca1ee1351f7e967810242a6977
Dian Yu <diayu@ucdavis.edu>
2019-01-22 14:51:09 -0800

    added pretrained_embedding file

---
+++ match_pretrained_embedding.py
@@ -0,0 +1,35 @@
+import torch
+
+vocab_dir = "vocab.pt"
+embedding_dir = "/home/dianyu/Downloads/crawl-300d-2M.vec"
+emb_dim = 300
+
+
+def match_embedding():
+    vocab = torch.load(vocab_dir)
+    embeddings = get_embeddings(embedding_dir)
+    load_embeddings = torch.rand(len(vocab), emb_dim)
+    count = {"match": 0, "miss": 0}
+
+    for w, w_id in vocab.word2index.items():
+        if w in embeddings:
+            load_embeddings[w_id] = embeddings[word]
+            count["match"] += 1
+        else:
+            count["miss"] += 1
+
+    print("%d match, %d miss" % (count["match"], count["miss"]))
+
+    return torch.Tensor(filtered_embeddings)
+
+
+def get_embeddings(embedding_file):
+    embeddings = dict()
+    for l in open(embedding_file, "rb").readlines():
+        l_split = l.decode('utf8'.strip().split())
+        if len(l_split) == 2:
+            continue
+        word, emb = l_split
+        embeddings[word] = [float(em) for em in emb]
+
+    return embeddings
