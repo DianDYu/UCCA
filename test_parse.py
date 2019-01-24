ff96eb1e720bfea4091d4f3b38eb5d4a107f2fc4
Dian Yu <diayu@ucdavis.edu>
2019-01-23 16:44:06 -0800

    added pos_tensor for input emb concat in model

--- test_parse.py
+++ test_parse.py
@@ -3,18 +3,21 @@ from parse import *
 
 dev_file_dir = "dev_proc.pt"
 vocab_dir = "vocab.pt"
+pos_vocab_dir = "pos_vocab.pt"
 
-checkpoint_path = "/home/dianyu/Desktop/P/UCCA/models/epoch_35_f1_77.68.pt"
+checkpoint_path = "/home/dianyu/Desktop/P/UCCA/models/epoch_216_f1_73.79.pt"
 
 
 def main():
     dev_ids, dev_text, dev_text_tensor, dev_passages, dev_linearized, \
-        dev_clean_linearized, dev_pos = loading_data(dev_file_dir)
+        dev_clean_linearized, dev_pos, dev_ent, dev_head = loading_data(dev_file_dir)
     vocab = torch.load(vocab_dir)
+    pos_vocab = torch.load(pos_vocab_dir)
+    pos_tensor = get_pos_tensor(pos_vocab, dev_pos)
 
     model_r, attn_r = load_test_model(checkpoint_path)
 
-    f1 = get_validation_accuracy(dev_text_tensor, model_r, attn_r, dev_text, dev_passages, dev_pos)
+    f1 = get_validation_accuracy(dev_text_tensor, model_r, attn_r, dev_text, dev_passages, dev_pos, pos_tensor)
 
     print("evaluated on %d passages" % len(dev_passages))
     print("micro F1: %.4f " % f1)
