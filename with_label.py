9b79eac4caf075c15cf8fd53daac8ea29c0c8d56
Dian Yu <diayu@ucdavis.edu>
2019-01-24 16:38:50 -0800

    new training implementation 1. with matrix multiplication instead of index prediction 2. added label

---
+++ with_label.py
@@ -0,0 +1,186 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def train_with_label(sent_tensor, clean_linearized, model, model_optimizer, a_model, a_model_optimizer,
+                     label_model, label_model_optimizer, criterion, ori_sent, pos_tensor):
+
+    model_optimizer.zero_grad()
+    a_model_optimizer.zero_grad()
+    label_model_optimizer.zero_grad()
+
+    max_recur = 5
+
+    max_grad_norm = 1.0
+
+    unit_loss = 0
+    label_loss = 0
+    unit_loss_num = 0
+    label_loss_num = 0
+
+    output, hidden = model(sent_tensor, pos_tensor)
+    # output: (seq_len, batch, hidden_size)
+    # output_2d: (seq_len, hidden_size)
+    # assume batch_size = 1
+    output_2d = output.squeeze(1)
+    # output_i: (1, hidden_size)
+    # output_i = output[i]
+
+    linearized_target = clean_linearized
+
+    index = 0
+    stack = []
+    new_node_embedding = []
+    index_label = []
+    i = 0
+
+    while i < len(linearized_target):
+        token = linearized_target[i]
+        ori_word = ori_sent[index] if index < len(ori_sent) else "<EOS>"
+
+        if token[0] == "[" and token[-1] != "*" and token[-1] != "]":
+            # PROPN: pre-processed with [X label. During training, just ignore the loss
+            if token == "[X" and linearized_target[i + 2] == "[X":
+                while True:
+                    if linearized_target[i + 2] == "[X":
+                        i += 2
+                        index += 1
+                    else:
+                        # not do anything here so that it will run with the same logic
+                        break
+                continue
+
+            else:
+                # condition when it is actually a terminal "["
+                stack.append(index)
+                index_label.append((index, token[1:]))
+
+        # ignore IMPLICIT edges
+        elif token == "IMPLICIT]":
+            stack.pop()
+            index_label.pop()
+            i += 1
+            continue
+
+        # terminal node
+        elif len(token) > 1 and token[-1] == "]":
+            if linearized_target[i + 1] != "]":
+                # attend to itself
+                assert token[:-1] == ori_word, "the terminal word: %s should " \
+                                               "be the same as ori_sent: %s" % (token[:-1], ori_word)
+                attn_weight = a_model(output[index], output_2d, index)
+                unit_loss += criterion(attn_weight, torch.tensor([index], dtype=torch.long, device=device))
+                unit_loss_num += 1
+            index += 1
+            stack.pop()
+            new_node_embedding.append(output[index])
+
+        # remote: ignore for now
+        elif token[0] == "[" and token[-1] == "*":
+            # the remote edge refer to a terminal node; or a node that is not an NER (ex.
+            # '[A*', '[R', 'with]', '[E', 'her]', '[C', 'father]', ']')
+
+            """[A*', '[A', '[E', 'Her]', '[C', 'parents]', ']', '[F', 'were]', 
+            '[P', 'criticized]', '[A', '[R', 'by]', '[E', 'the]', '[C', 'community]', ']',
+             '[A', '[R', 'for]', '[E', 'their]', '[E', 'progressive]', '[C', 'views]', ']', ']'"""
+
+            if linearized_target[i + 1][0] != "[":
+                i += 2
+            else:
+                remote_stack = [i]
+                # the remote edge may refer to a node with arbitrary length
+                while True:
+                    i += 1
+                    # consider cases like "[]" where the first one is one terminal in the passage
+                    if linearized_target[i][0] == "[" and linearized_target[i][-1] != "]":
+                        remote_stack.append(i)
+                    elif linearized_target[i][-1] == "]" and linearized_target[i][0] != "[":
+                        remote_stack.pop()
+                    if len(remote_stack) == 0:
+                        i += 1
+                        break
+
+            continue
+
+        # close a node
+        elif token == "]":
+            current_index = index - 1
+            left_border = stack.pop()
+            attn_weight = a_model(output[current_index], output_2d, current_index)
+            unit_loss += criterion(attn_weight, torch.tensor([left_border], dtype=torch.long, device=device))
+            unit_loss_num += 1
+
+            children_enc_label = []
+            for ind, label in index_label.pop():
+                enc = new_node_embedding.pop()
+                children_enc_label.append((enc, label))
+                if ind == left_border:
+                    break
+            new_node_embedding.append(output[current_index] - output[left_border])
+
+            parent_enc = 0
+            for child_enc, child_label in children_enc_label:
+                label_weight = label_model(parent_enc, child_enc)
+                label_loss += criterion(label_weight, child_label)
+                label_loss_num += 1
+
+            for r in range(1, max_recur + 1):
+                node_output = output[current_index] - output[left_border]
+                node_attn_weight = a_model(node_output, output_2d, current_index)
+
+                if i + 1 < len(linearized_target):
+                    if linearized_target[i + 1] == "]":
+                        left_border = stack.pop()
+                        unit_loss += criterion(node_attn_weight,
+                                               torch.tensor([left_border], dtype=torch.long, device=device))
+                        unit_loss_num += 1
+
+                        # label prediction
+                        children_enc_label = []
+                        for ind, label in index_label.pop():
+                            enc = new_node_embedding.pop()
+                            children_enc_label.append((enc, label))
+                            if ind == left_border:
+                                break
+                        new_node_embedding.append(output[current_index] - output[left_border])
+
+                        parent_enc = 0
+                        for child_enc, child_label in children_enc_label:
+                            label_weight = label_model(parent_enc, child_enc)
+                            label_loss += criterion(label_weight, child_label)
+                            label_loss_num += 1
+
+                        i += 1
+                    else:
+                        unit_loss += criterion(node_attn_weight,
+                                               torch.tensor([current_index], dtype=torch.long, device=device))
+                        unit_loss_num += 1
+                        break
+                else:
+                    break
+
+        i += 1
+
+        word_stack = [ori_sent[i] for i in stack]
+        assert len(stack) == 0, "stack is not empty, left %s" % word_stack
+
+        unit_loss.backward()
+        label_loss.backward()
+
+        # gradient clipping
+        torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=max_grad_norm)
+        torch.nn.utils.clip_grad_norm_(parameters=a_model.parameters(), max_norm=max_grad_norm)
+        torch.nn.utils.clip_grad_norm_(parameters=label_model.parameters(), max_norm=max_grad_norm)
+
+        model_optimizer.step()
+        a_model_optimizer.step()
+        label_model_optimizer.step()
+
+        return unit_loss.item() / unit_loss_num + label_loss.item() / label_loss_num
+
+
+def evaluate_with_label():
+    pass
