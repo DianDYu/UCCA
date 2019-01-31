ea625a90b577f057f99474eb738131a0f0ac18d1
Dian Yu <diayu@ucdavis.edu>
2019-01-30 19:13:47 -0800

    added io functions to save/load ent_type and case info

--- io_file.py
+++ io_file.py
@@ -13,7 +13,6 @@ label2index = {}
 for label in labels:
     label2index[label] = len(label2index)
 
-
 # data reader from xml
 def read_passages(file_dirs):
     return ioutil.read_files_and_dirs(file_dirs)
@@ -30,6 +29,8 @@ def tensorFromSentence(vocab, sentence):
 
 def prepareData(vocab, text):
     for sent in text:
+        if len(sent) == 0:
+            sent = "NA"
         vocab.addSentence(sent)
     return vocab
 
@@ -49,6 +50,15 @@ def get_text(passages):
 
 def get_pos_tensor(vocab, pos):
     return [tensorFromSentence(vocab, tags) for tags in pos]
+
+
+def get_ent_tensor(vocab, ent):
+    return [tensorFromSentence(vocab, ent_type) for ent_type in ent]
+
+
+def get_case_tensor(case):
+    case2idx = [1 if word_case else 0 for word_case in case]
+    return [torch.tensor(case_idx, dtype=torch.long, device=device).view(-1, 1) for case_idx in case2idx]
 
 
 def loading_data(file_dir):
@@ -90,7 +100,7 @@ def loading_data_passsage(file_dir):
         sent_pos, sent_ent, sent_head
 
 
-def passage_preprocess_data(train_passages, train_file_dir, dev_passages, dev_file_dir, vocab_dir):
+def passage_preprocess_data(train_passages, train_file_dir, dev_passages, dev_file_dir, vocab_dir, use_lowercase=False):
     vocab = Vocab()
     train_text = get_text(train_passages)
     dev_text = get_text(dev_passages)
@@ -116,13 +126,20 @@ def passage_preprocess_data(train_passages, train_file_dir, dev_passages, dev_fi
 
             num_sents += 1
 
+            case_info = [word[0].isupper() for word in ori_sent.split()]
+
+            if use_lowercase:
+                ori_sent = ori_sent.lower()
+
             new_line_data.append(sent_id)
             new_line_data.append(ori_sent)
             new_line_data.append(sent_tensor)
             new_line_data.append(sent_passage)
             new_line_data.append([node.extra["pos"] for node in l0.all])
-            new_line_data.append([node.extra["ent_type"] for node in l0.all])
+            new_line_data.append([node.extra["ent_type"] if len(node.extra["ent_type"]) > 0
+                                  else "NA" for node in l0.all])
             new_line_data.append([node.extra["head"] for node in l0.all])
+            new_line_data.append(case_info)
 
             data_list.append(new_line_data)
 
@@ -136,11 +153,11 @@ def passage_preprocess_data(train_passages, train_file_dir, dev_passages, dev_fi
 
 def passage_loading_data(file_dir):
     sent_ids, data_text, data_text_tensor, passages, sent_pos, \
-        sent_ent, sent_head = [], [], [], [], [], [], []
+        sent_ent, sent_head, sent_case = [], [], [], [], [], [], [], []
     data_list = torch.load(file_dir)
 
     for line in data_list:
-        (sent_id, ori_sent, sent_tensor, passage, pos, ent, head)\
+        (sent_id, ori_sent, sent_tensor, passage, pos, ent, head, case)\
             = [i for i in line]
         sent_ids.append(sent_id)
         data_text.append(ori_sent)
@@ -149,9 +166,10 @@ def passage_loading_data(file_dir):
         sent_pos.append(pos)
         sent_ent.append(ent)
         sent_head.append(head)
+        sent_case.append(case)
 
     return sent_ids, data_text, data_text_tensor, passages, \
-        sent_pos, sent_ent, sent_head
+        sent_pos, sent_ent, sent_head, sent_case
 
 
 def prepare_pos_vocab(train_pos, dev_pos, pos_vocab_dir):
@@ -162,7 +180,15 @@ def prepare_pos_vocab(train_pos, dev_pos, pos_vocab_dir):
     torch.save(pos_vocab, pos_vocab_dir)
 
 
-def save_test_model(model_e, a_model_e, label_model_e, s_model_e, n_words, pos_n_words, epoch, f1):
+def prepare_ent_vocab(train_pos, dev_pos, ent_vocab_dir):
+    ent_vocab = Vocab()
+    ent_vocab = prepareData(ent_vocab, train_pos)
+    ent_vocab = prepareData(ent_vocab, dev_pos)
+
+    torch.save(ent_vocab, ent_vocab_dir)
+
+
+def save_test_model(model_e, a_model_e, label_model_e, s_model_e, n_words, pos_n_words, ent_n_words, epoch, f1):
     if not isinstance(s_model_e, str):
         s_model_dict = s_model_e.state_dict()
     else:
@@ -174,6 +200,7 @@ def save_test_model(model_e, a_model_e, label_model_e, s_model_e, n_words, pos_n
         'label_model': label_model_e.state_dict(),
         's_model': s_model_dict,
         'vocab_size': n_words,
-        'pos_vocab_size': pos_n_words
+        'pos_vocab_size': pos_n_words,
+        'ent_vocab_size': ent_n_words
     }
     torch.save(checkpoint, "models/epoch_%d_f1_%.2f.pt" % (epoch, f1 * 100))
